e47407b89cc10fa878062dd3f3acf34a3cb60cec
hsiao <uuu510294@gmail.com>
2024-10-08 10:54:40 +0800

    1008_11am

--- final-vue-front/src/components/layout/inst.vue
+++ final-vue-front/src/components/layout/inst.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="instagram-section">
-      <h2>Follow Us On Instagram!</h2>
+      <h2>來Instagram追蹤我們唷!</h2>
       <div class="instagram-grid">
         <div v-for="post in instagramPosts" :key="post.id" class="instagram-post">
           <img :src="post.media_url" alt="Instagram Post" />
@@ -27,28 +27,28 @@
     instagramPosts.value = [
       {
         id: 1,
-        media_url: 'https://scontent.cdninstagram.com/v/t51.29350-15/447843429_993081762283004_7856535013924751952_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=18de74&_nc_ohc=vBEugAeut34Q7kNvgHhnRYC&_nc_ht=scontent.cdninstagram.com&edm=AM6HXa8EAAAA&_nc_gid=Accg6nsA-G6cYyyKssnjQsx&oh=00_AYBxPI7yqOovXLQspsM0xJVLNaZn9c4socny0gySf0dPQA&oe=67089664',
-        caption: '越野車比賽好刺激!那天天氣超好...'
+        media_url: '/public/i1.jpg',
+        caption: '想念北海道的海邊與夕陽....'
       },
       {
         id: 2,
-        media_url: 'https://scontent.cdninstagram.com/v/t51.29350-15/445550962_1171127580703623_8114276794702001926_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=18de74&_nc_ohc=ztB0zV6tcPEQ7kNvgEW-_dS&_nc_ht=scontent.cdninstagram.com&edm=AM6HXa8EAAAA&_nc_gid=Accg6nsA-G6cYyyKssnjQsx&oh=00_AYBzMN5SW1lrfRCLJIVhD1RAUsbR4GfSHIAV8jikd-nG6g&oe=67089842',
-        caption: '泛舟超好玩的雖然很冷.....'
+        media_url: '/public/i2.jpg',
+        caption: '真的有外星人嗎?金字塔好高啊!!!...'
       },
       {
         id: 3,
-        media_url: 'https://scontent.cdninstagram.com/v/t51.29350-15/445418207_1742030389931040_2412169838932498547_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=18de74&_nc_ohc=H0h1-G6gjnYQ7kNvgHlTHjX&_nc_ht=scontent.cdninstagram.com&edm=AM6HXa8EAAAA&_nc_gid=AGk6hBhJSf5i1By7SgVrhfc&oh=00_AYCZrjwJSyTgaQMhPgYIEqK5yCqPMvvciaa60cGKH-Y6Ug&oe=6709523A',
-        caption: '我跟我媽一起租腳踏!我媽體力比我好....'
+        media_url: '/public/i3.jpg',
+        caption: '謝謝趣露營教練帶我們去潛水~那天天氣超好的!好愛!'
       },
       {
         id: 4,
-        media_url: 'https://scontent.cdninstagram.com/v/t51.29350-15/449757210_3956269338031400_7085097138128283998_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=18de74&_nc_ohc=xU322CAfXP4Q7kNvgF1Oz_5&_nc_ht=scontent.cdninstagram.com&edm=AM6HXa8EAAAA&_nc_gid=AGk6hBhJSf5i1By7SgVrhfc&oh=00_AYBWKKznHcAwILndwDBUTUuCpuVn06fSTy13xjtN64if5A&oe=67094CDE',
-        caption: '去美國比賽我很緊張......'
+        media_url: '/public/i4.jpg',
+        caption: '謝謝趣露營辦的滑雪行程!超好玩的!我滑到腿痠了!'
       },
       {
         id: 5,
-        media_url: 'https://scontent.cdninstagram.com/v/t51.29350-15/446100056_3822401937980275_2356870262720441223_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=18de74&_nc_ohc=chnxrPpWK1YQ7kNvgHAdCDQ&_nc_ht=scontent.cdninstagram.com&edm=AM6HXa8EAAAA&_nc_gid=AGk6hBhJSf5i1By7SgVrhfc&oh=00_AYBpVCSmGiqjLCgdIt0b2PPALqkxk1sIMXCi9FUNshmNPw&oe=67097796',
-        caption: '大家快來!臨時快閃攤位~~唷'
+        media_url: '/public/i55.jpg',
+        caption: '台灣也可以划水喔!!還有裝備提供....'
       }
     ];
   });
